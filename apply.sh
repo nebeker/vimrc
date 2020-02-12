935c20f6a8f712181d8c099b9a0e78fb0066507e
Nuno Nebeker <nunonebeker@gmail.com>
2020-02-12 15:13:47 +0000

    Folder -> directory; announce action; sh - bash

--- apply.sh
+++ apply.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-read -p "Copy vimrc to home folder and replace current configuration (Y/N)?" -n 1 -r
+read -p "Copy vimrc to home directory and replace current configuration (Y/N)?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     cp ./vimrc ~/.vimrc
+    echo "Done!"
+else
+    echo "Ëxiting."
 fi
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 This is my simple .vimrc configuration with basic appearance tweaks and handy key mappings.
 
-The `apply.sh` scrpt quickly copies the file to the user's home folder.
+The `apply.sh` scrpt quickly copies the file to the user's home directory.
